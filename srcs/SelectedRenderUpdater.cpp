408415ef76ac137052a30ac241bd24b09fcbac1c
Vivien <bonnet.vivien@gmail.com>
2022-05-31 10:52:29 +0200

    Selectable box depends of unit orientation.

--- srcs/SelectedRenderUpdater.cpp
+++ srcs/SelectedRenderUpdater.cpp
@@ -6,6 +6,7 @@
 #include "hatcher/Graphics/MeshBuilder.hpp"
 #include "hatcher/Graphics/RenderUpdater.hpp"
 
+#include "Movement2DComponent.hpp"
 #include "Position2DComponent.hpp"
 #include "Selectable2DComponent.hpp"
 
@@ -46,16 +47,24 @@ public:
     {
         auto selectableComponents = renderComponentManager->GetComponents<Selectable2DComponent>();
         auto positionComponents = componentManager->GetComponents<Position2DComponent>();
+        auto movementComponents = componentManager->GetComponents<Movement2DComponent>();
         HATCHER_ASSERT(selectableComponents.size() == positionComponents.size());
+        HATCHER_ASSERT(selectableComponents.size() == movementComponents.size());
         for (hatcher::uint i = 0; i < selectableComponents.size(); i++)
         {
             const std::optional<Selectable2DComponent>& selectableComponent =
                 selectableComponents[i];
             const std::optional<Position2DComponent>& positionComponent = positionComponents[i];
+            const std::optional<Movement2DComponent>& movementComponent = movementComponents[i];
             if (selectableComponent && selectableComponent->selected)
             {
-                const glm::mat4 modelMatrix =
-                    glm::translate(glm::vec3(positionComponent->position, 0.f));
+                const float angle =
+                    glm::orientedAngle(glm::vec2(1.f, 0.f), movementComponent->orientation);
+                glm::mat4 modelMatrix = glm::mat4(1.f);
+                modelMatrix =
+                    glm::translate(modelMatrix, glm::vec3(positionComponent->position, 0.f));
+                modelMatrix = glm::rotate(modelMatrix, angle, glm::vec3(0.f, 0.f, 1.f));
+
                 const hatcher::Box2f selectionBox =
                     frameRenderer.ProjectBox3DToWindowCoords(selectableComponent->box, modelMatrix);
 
