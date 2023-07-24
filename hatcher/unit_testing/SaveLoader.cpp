ac492d292c824e8650c9a5fed434958ee643b46e
Vivien <bonnet.vivien@gmail.com>
2023-07-24 21:45:19 +0200

    Added some start of unit-testing for specific parts.

---
+++ hatcher/unit_testing/SaveLoader.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <limits>
+
+#include "hatcher/ComponentLoader.hpp"
+#include "hatcher/ComponentSaver.hpp"
+
+using namespace hatcher;
+
+template <typename T>
+int testEquals(const T& a, const T& b)
+{
+    if (a != b)
+        std::cerr << "unit test fail: " << a << " != " << b << std::endl;
+    return a != b;
+}
+
+int testInt()
+{
+    int input[] = {
+        3,
+        42,
+        std::numeric_limits<int>::min(),
+        std::numeric_limits<int>::max(),
+    };
+    constexpr int testCount = std::size(input);
+    int output[testCount];
+
+    ComponentSaver saver;
+    for (int i = 0; i < testCount; i++)
+        saver << input[i];
+
+    ComponentLoader loader(saver.Result());
+    for (int i = 0; i < testCount; i++)
+        loader << output[i];
+
+    int fails = 0;
+    for (int i = 0; i < testCount; i++)
+        fails += testEquals(output[i], input[i]);
+
+    return fails;
+}
+
+int testFloat()
+{
+    float input[] = {
+        0.f,
+        42.f,
+        8.3f,
+        -78.f,
+    };
+    constexpr int testCount = std::size(input);
+    float output[testCount];
+
+    ComponentSaver saver;
+    for (int i = 0; i < testCount; i++)
+        saver << input[i];
+
+    ComponentLoader loader(saver.Result());
+    for (int i = 0; i < testCount; i++)
+        loader << output[i];
+
+    int fails = 0;
+    for (int i = 0; i < testCount; i++)
+        fails += testEquals(output[i], input[i]);
+
+    return fails;
+}
+
+int main()
+{
+    int fails = 0;
+
+    fails += testInt();
+    fails += testFloat();
+
+    return fails;
+}
