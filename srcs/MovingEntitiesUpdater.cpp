1bb693e579a7e4e82a5a864e5c95f012541ff5b2
Vivien <bonnet.vivien@gmail.com>
2023-08-12 17:55:51 +0200

    Fix: MovingEntitiesUpdater doesn't "stop" for a tick an entity at each path step.

--- srcs/MovingEntitiesUpdater.cpp
+++ srcs/MovingEntitiesUpdater.cpp
@@ -23,23 +23,30 @@ public:
             if (movements[i])
             {
                 HATCHER_ASSERT(positions[i]);
-                const float movementLength = 0.05f;
+                float movementLength = 0.05f;
                 Movement2DComponent& movement2D = *movements[i];
                 Position2DComponent& position2D = *positions[i];
                 if (!movement2D.path.empty())
                 {
-                    const glm::vec2 nextObjective = movement2D.path.back();
-                    const glm::vec2 direction = (nextObjective - positions[i]->position);
-                    position2D.orientation = glm::normalize(movement2D.path.back() - position2D.position);
-                    if (glm::length(direction) < movementLength)
+                    const glm::vec2 startPosition = position2D.position;
+                    while (!movement2D.path.empty() && movementLength > 0.f)
                     {
-                        position2D.position = nextObjective;
-                        movement2D.path.pop_back();
+                        glm::vec2 nextObjective = movement2D.path.back();
+                        const glm::vec2 direction = (nextObjective - position2D.position);
+                        const float distanceToNextObjective = glm::length(direction);
+                        if (distanceToNextObjective <= movementLength)
+                        {
+                            position2D.position = nextObjective;
+                            movement2D.path.pop_back();
+                            movementLength -= distanceToNextObjective;
+                        }
+                        else
+                        {
+                            position2D.position += glm::normalize(direction) * movementLength;
+                            movementLength = 0.f;
+                        }
                     }
-                    else
-                    {
-                        position2D.position += glm::normalize(direction) * movementLength;
-                    }
+                    position2D.orientation = glm::normalize(position2D.position - startPosition);
                 }
             }
         }
