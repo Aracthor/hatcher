f441d0df1ae3a61c38fce92ef1e3ba3f9f9a711e
Vivien <bonnet.vivien@gmail.com>
2024-06-09 02:26:20 +0200

    Unnamed proto: Forget about the "line mesh" of static meshes.

--- unnamed_proto/srcs/RenderUpdaters/StaticMeshRenderUpdater.cpp
+++ unnamed_proto/srcs/RenderUpdaters/StaticMeshRenderUpdater.cpp
@@ -33,13 +33,10 @@ public:
 
         m_fillMaterial = materialFactory->CreateMaterial("shaders/hello_world_3D.vert", "shaders/hello_texture.frag");
         m_fillMaterial->AddTexture("uniTexture", lockerTexture);
-        m_lineMaterial = materialFactory->CreateMaterial("shaders/hello_world_3D.vert", "shaders/hello_color.frag");
-        m_lineMaterial->AddUniform("uniColor", glm::vec4(0.0, 0.0, 0.0, 1.0));
 
         const char* lockerFileName = "assets/meshes/locker.obj";
-        StaticMeshes& lockerMeshes = m_meshes[StaticMeshComponent::Locker];
-        lockerMeshes.first = meshLoader->LoadWavefront(m_fillMaterial.get(), lockerFileName, Primitive::Triangles);
-        lockerMeshes.second = meshLoader->LoadWavefront(m_lineMaterial.get(), lockerFileName, Primitive::Lines);
+        m_meshes[StaticMeshComponent::Locker] =
+            meshLoader->LoadWavefront(m_fillMaterial.get(), lockerFileName, Primitive::Triangles);
     }
 
     ~StaticMeshRenderUpdater() = default;
@@ -56,26 +53,21 @@ public:
             if (positionComponents[i] && staticMeshComponents[i])
             {
                 HATCHER_ASSERT(staticMeshComponents[i]->type < StaticMeshComponent::COUNT);
-                const StaticMeshes& meshes = m_meshes[staticMeshComponents[i]->type];
+                const unique_ptr<Mesh>& mesh = m_meshes[staticMeshComponents[i]->type];
                 if (selectableComponents[i] && selectableComponents[i]->box.IsEmpty())
                 {
-                    selectableComponents[i]->box.Add(meshes.first->Box().Scaled(1.1f));
+                    selectableComponents[i]->box.Add(mesh->Box().Scaled(1.1f));
                 }
 
                 glm::mat4 modelMatrix = TransformationHelper::ModelFromComponents(positionComponents[i]);
-                frameRenderer.AddMeshToRender(meshes.first.get(), modelMatrix);
-                modelMatrix = glm::scale(modelMatrix, glm::vec3(1.01f, 1.01f, 1.01f));
-                frameRenderer.AddMeshToRender(meshes.second.get(), modelMatrix);
+                frameRenderer.AddMeshToRender(mesh.get(), modelMatrix);
             }
         }
     }
 
 private:
-    using StaticMeshes = std::pair<unique_ptr<Mesh>, unique_ptr<Mesh>>;
-
     unique_ptr<Material> m_fillMaterial;
-    unique_ptr<Material> m_lineMaterial;
-    StaticMeshes m_meshes[StaticMeshComponent::COUNT] = {};
+    unique_ptr<Mesh> m_meshes[StaticMeshComponent::COUNT] = {};
 };
 
 RenderUpdaterRegisterer<StaticMeshRenderUpdater> registerer;
