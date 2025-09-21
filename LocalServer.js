9fd259dfef7b8f6fd41426d16ef4a42525cd9eef
Vivien <bonnet.vivien@gmail.com>
2025-09-21 03:26:07 +0200

    LocalServer: Useless require.

--- LocalServer.js
+++ LocalServer.js
@@ -1,9 +1,7 @@
-
 const http  = require('http');
 const url   = require('url');
 const fs    = require('fs');
 const path  = require('path');
-const os    = require('os');
 const port  = parseInt(process.argv[2] || 4242, 10);
 
 // maps file extention to MIME typere
